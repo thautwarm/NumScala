4e7a1cb014efa6fb3f21fdb491f56f278b610c94
thautwarm <twshere@outlook.com>
2017-06-19 20:53:19 +0800

    init

--- ReadMe.rst
+++ ReadMe.rst
@@ -4,7 +4,7 @@ NumScala
 
 For scientific research and data analysis, we ask for a framework which is scalable as well as lightweight.
 
-Like *Pandas* in Python, *Dataframe* in R.
+Like *Pandas* in Python, or *Dataframe* in R.
 
 Requirements
 ----------
--- util/getZipped.scala
+++ util/getZipped.scala
@@ -27,20 +27,20 @@ object xmlParser {
 object getZipped {
     def unzipXlsxFile(input:String,output:String,dictArgs:immuMap[String,Any]=immuMap()):Unit={
         /*unzip a xlsx file into a directory */
-        
+
         //deal with default arguments
         val filter =  if(dictArgs.isDefinedAt("filter"))  dictArgs("filter").asInstanceOf[String=>Boolean] else null
         val verbose = if(dictArgs.isDefinedAt("verbose")) dictArgs("verbose").asInstanceOf[Boolean] else false
-        
+
         //some initial arguments
         val outputFix:String =  if (output.endsWith("/")) output else output+"/" /* deal with the output directory */
         val pass=null  /* something like keywords which might look graceful */
-        var zipInput:ZipInputStream =new ZipInputStream( 
+        var zipInput:ZipInputStream =new ZipInputStream(
                                   new FileInputStream(
                                   new File(input)    ))
         var zipFile:ZipFile = new ZipFile(input)
-       
-        
+
+
         //iterator formed as cycle.
         while(null !=  /* if the input-stream is not empty, do works. */
          ((zipin:ZipInputStream)=>{
@@ -50,14 +50,14 @@ object getZipped {
               if (verbose) println(entry.getName)
               ((file:File)=> {
               if (filter!=null && !filter(file.getAbsolutePath()) ) {pass}
-              else if  (   
+              else if  (
                    ( if (! file.getParentFile().exists() ){file.getParentFile().mkdirs();true} else false)
-                   || 
+                   ||
                    (!file.exists())                       /*for optimization*/
                   )
-                { 
+                {
                  try{
-                   /* some item in the zipped file doesn't exist really   
+                   /* some item in the zipped file doesn't exist really
                     * so use try to ignore
                     */
                    file.createNewFile();
@@ -70,7 +70,7 @@ object getZipped {
                     }
                   catch
                     {
-                     case e:IOException=>{pass} 
+                     case e:IOException=>{pass}
                           /* if this item doesn't exists, ignore it.*/
                      case _:Throwable=> throws
                      }
@@ -80,28 +80,28 @@ object getZipped {
            entry
           }
           })(zipInput)
-          ){/*pass*/}   
+          ){/*pass*/}
 
         zipFile.close()
         zipInput.close()
     }
-    
+
     def parseXlsx(path:String)={
-      
+
       val pathStructure:Array[String] = path.split("/")
       val parentDirectory = refArrayOps(refArrayOps(pathStructure).slice(1, pathStructure.length-1)).toList.foldLeft(pathStructure(0))(_+"/"+_)
       var tempPath= refArrayOps(pathStructure).last.replace(".xlsx", "$")
       var CurrentDir = refArrayOps(refArrayOps(new File(parentDirectory).listFiles()).map(_.getName())).toSet
-      
+
       while (CurrentDir.contains(tempPath)) tempPath+="$"  /*geneate a file whose name is not reduplicated*/
-      
+
       tempPath = parentDirectory+"/"+tempPath
       unzipXlsxFile(path,tempPath,immuMap("filter"->((x:String)=> x.contains("xl\\worksheets")|| x.contains("xl\\sharedStrings.xml") )))
-      
+
       val sharedString =tempPath + "/xl/sharedStrings.xml"
       val sourcePath=tempPath+"/xl/worksheets/"
       val taskFile:File = new File(sourcePath)
-      
+
       val ret = parseXmlFile (refArrayOps(taskFile.listFiles()).filter( (x:File)=> x.getName().endsWith(".xml")), new File(sharedString) )
       dirDeal.delete(new File(tempPath))
       ret
@@ -109,14 +109,14 @@ object getZipped {
 
     def parseXmlFile(files:Array[File],shareStringFile:File):Array[Array[Array[String]]]={
       refArrayOps(files).map(xmlParser.getData(_,shareStringFile))
-     
-      
+
+
     }
-    def main(args:Array[String]):Unit={  //Test 
+    def main(args:Array[String]):Unit={  //Test
       var df =parseXlsx("C:/Users/thautwarm/Desktop/demo/p.xlsx").map(_.map(_.toList).toList).toList
       println(df)
 //      unzipXlsxFile("C:/Users/thautwarm/Desktop/demo/mas.xlsx","C:/Users/thautwarm/Desktop/demo/mas_unzip",immuMap("filter"->((x:String)=> x.contains("xl\\worksheets"))))
       }
-    
-    
+
+
 }
