22a9c3bc4b48c824a77ae268c56a1a590493e668
thautwarm <twshere@outlook.com>
2017-06-28 02:56:15 +0800

    moe..

--- src/user/dataFrame.scala
+++ src/user/dataFrame.scala
@@ -1,37 +1,63 @@
 package user
 import scala.language.implicitConversions
-import scala.reflect.{ClassTag,classTag}
+import scala.reflect.{ ClassTag, classTag }
 import util.config._
 import util.baseCalc
 import math.pow
 
-
-
 object dataFrame {
-  abstract class Series[T]{
-     var name:String
-     val data:Vector[T]
+  abstract class Series[T] {
+    var name: String
+    val data: Vector[T]
+    
   }
-  
-  trait countable[T] extends Series[T]{
-    def count (f:T=>Boolean):Int=data.count(f)
-    def frequency [K](f:T=>K):Map[K,Double]=data.groupBy(f).mapValues((x:Vector[T])=> 1.0*x.length/data.length)
-    def unique ():Set[T]=data.toSet
+  trait hasNaN[T] extends Series[T]{
+    def nanNum ()= data.count((x: T) => x == null)
+    def tolNum ()= data.length
+    def notNanNum ()= tolNum - nanNum
   }
-  
+  trait countable[T] extends Series[T] {
+    def count(f: T => Boolean): Int = data.count(f)
+    def frequency[K](f: T => K): Map[K, Double] = data.groupBy(f).mapValues((x: Vector[T]) => 1.0 * x.length / data.length)
+    def unique(): Set[T] = data.toSet
+  }
+
+  abstract class Numerical[T <% Double: Numeric: ClassTag] extends Series[T] with hasNaN[T] {
+    def sum(isPar: Boolean = false): T = baseCalc.sum(data, isPar)
+    def nansum(isPar: Boolean = false): T = baseCalc.nansum(data, isPar)
+
+    def mean(isPar: Boolean = false): Double = sum(isPar) / tolNum
+    def nanmean(isPar: Boolean = false): Double = nansum(isPar) / notNanNum
+
+    //for n-order center moment . variance is the  "2-order center moment"
+    def ocm(order: Int, isPar: Boolean = false): Double = {
+      val meanVal: Double = mean(isPar)
+      val f = (x: T) => x - meanVal
+      baseCalc.sumWith(data, f, isPar) / tolNum
+    }
+    def nanocm(order: Int, isPar: Boolean = false): Double = {
+      val meanVal: Double = mean(isPar)
+      val f = (x: T) => x - meanVal
+      baseCalc.nansumWith(data, f, isPar) / notNanNum
+    }
+
+  }
+
   class LiteralSeries(source: Vector[Any], index: Boolean = false) extends Series[String] with countable[String] {
     var name: String = ""
     val data: Vector[String] =
       if (source.length == 0) { name = "empty"; Vector[String]() }
       else {
         (index match {
-          case true => name = source.head.toString ; source.tail
+          case true =>
+            name = source.head.toString; source.tail
           case false => name = "unname"; source
-         }) then (_.map(_.toString))
+        }) then (_.map(_.toString))
       }
     //I feel sooooooooooooooooooooo charming!
   }
-  class NumericSeries[T <% Double: Numeric:ClassTag](source: Vector[Any], index: Boolean = false) extends countable[T]{
+
+  class NumericSeries[T <% Double: Numeric: ClassTag](source: Vector[Any], index: Boolean = false) extends Numerical[T] with countable[T] {
     var name: String = ""
     val data: Vector[T] = if (source.length == 0) { name = "empty"; Vector[T]() }
     else
@@ -46,27 +72,6 @@ object dataFrame {
           case ClassTag.Long   => _.map(_.toString then baseCalc.Str2Long).asInstanceOf[Vector[T]]
         }
       }
-    var nanNum = data.count((x: T) => x == null)
-    var tolNum = data.length
-    var notNanNum = tolNum - nanNum
-
-    def sum(isPar: Boolean = false): T = baseCalc.sum(data, isPar)
-    def nansum(isPar: Boolean = false): T = baseCalc.nansum(data, isPar)
-
-    def mean(isPar: Boolean = false): Double = sum(isPar) / tolNum
-    def nanmean(isPar: Boolean = false): Double = nansum(isPar) / tolNum
-
-    //for n-order center moment . variance is the  "2-order center moment"
-    def ocm(order: Int, isPar: Boolean = false): Double = {
-      val meanVal: Double = mean(isPar)
-      val f = (x: T) => x - meanVal
-      baseCalc.sumWith(data, f, isPar)
-    }
-    def nanocm(order: Int, isPar: Boolean = false): Double = {
-      val meanVal: Double = mean(isPar)
-      val f = (x: T) => x - meanVal
-      baseCalc.nansumWith(data, f, isPar)
-    }
 
   }
 
